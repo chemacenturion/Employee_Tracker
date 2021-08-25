857689598bb8edb8d881d14985175e28cff82998
chemacenturion <centurion.philadelphia@gmail.com>
2021-08-25 18:29:32 -0400

    updating with connection.js and attempting to gitignore .env file

---
+++ db/connections.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS
+});
